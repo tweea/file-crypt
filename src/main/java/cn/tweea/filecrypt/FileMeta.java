e7d50659c98a019c7ef68166d698622deec20d30
Tweea <tweea@qq.com>
2026-06-22 22:34:40 +0800

    增加位置字段

--- src/main/java/cn/tweea/filecrypt/FileMeta.java
+++ src/main/java/cn/tweea/filecrypt/FileMeta.java
@@ -4,10 +4,19 @@
  */
 package cn.tweea.filecrypt;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class FileMeta {
     private String plainFileName;
 
     private String cipherFileName;
+
+    private List<String> locations;
+
+    public FileMeta() {
+        this.locations = new ArrayList<>();
+    }
 
     public String getPlainFileName() {
         return plainFileName;
@@ -23,5 +32,13 @@ public class FileMeta {
 
     public void setCipherFileName(String cipherFileName) {
         this.cipherFileName = cipherFileName;
+    }
+
+    public List<String> getLocations() {
+        return locations;
+    }
+
+    public void setLocations(List<String> locations) {
+        this.locations = locations;
     }
 }
